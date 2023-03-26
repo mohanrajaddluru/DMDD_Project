0b9ab925fca4bf4db481e45d605d9d2829ff915f
Saigurunadh-2269 <konkapaka.s@northeastern.edu>
2023-03-25 22:44:36 -0400

    Update app_admin Script

--- 3_application_admin.sql
+++ 3_application_admin.sql
@@ -22,12 +22,12 @@ BEGIN
                 DBMS_OUTPUT.PUT_LINE('Currently user is connected cannot be deleted');
             END IF;
         END;
-    ELSE 
-        EXECUTE IMMEDIATE 'CREATE USER sales_executive IDENTIFIED BY "Sales_Password@321"';
-        DBMS_OUTPUT.PUT_LINE('User CREATED successfully.');
-        EXECUTE IMMEDIATE 'GRANT CREATE SESSION TO sales_executive';
-        EXECUTE IMMEDIATE 'GRANT INSERT, SELECT, UPDATE, DELETE ON application_admin.discounts TO sales_executive';
-        DBMS_OUTPUT.PUT_LINE('User granted privileges successfully.');
+    --ELSE 
+        --EXECUTE IMMEDIATE 'CREATE USER sales_executive IDENTIFIED BY "Sales_Password@321"';
+        --DBMS_OUTPUT.PUT_LINE('User CREATED successfully.');
+        --EXECUTE IMMEDIATE 'GRANT CREATE SESSION TO sales_executive';
+        --EXECUTE IMMEDIATE 'GRANT INSERT, SELECT, UPDATE, DELETE ON application_admin.discounts TO sales_executive';
+        --DBMS_OUTPUT.PUT_LINE('User granted privileges successfully.');
         
    END IF;    
 END;
